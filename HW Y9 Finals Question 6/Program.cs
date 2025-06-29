8f48f3778dddec7b497411329e4a38753a0316ca
Arthur Dye <22ADye@heckgrammar.co.uk>
2025-06-29 09:11:02 +0100

    //

--- HW Y9 Finals Question 6/Program.cs
+++ HW Y9 Finals Question 6/Program.cs
@@ -9,23 +9,29 @@ namespace HW_Y9_Finals_Question_6
             Dictionary<string, string> loginDetails = new Dictionary<string, string>();
             loginDetails.Add("Yusuf5", "33kk");
             loginDetails.Add("Mary80", "af5r");
-            string value = "";
-
 
             bool valid = false;
             do
             {
                 Console.WriteLine("Enter a username.");
                 string username = Console.ReadLine();
-                Console.WriteLine("Enter a password.");
-                string password = Console.ReadLine();
                 if (loginDetails.ContainsKey(username))
                 {
-                    if (loginDetails.TryGetValue(username, out value))
+                    Console.WriteLine("Enter a password.");
+                    string password = Console.ReadLine();
+                    if (loginDetails[username] == password)
                     {
-
+                        Console.WriteLine("Access granted.");
+                        valid = true;
+                    } else
+                    {
+                        Console.WriteLine("Username and password do not match. Please try again.");
                     }
+                } else
+                {
+                    Console.WriteLine("Username could not be found. Please try again.");
                 }
+                
             } while (!valid);
         }
     }
